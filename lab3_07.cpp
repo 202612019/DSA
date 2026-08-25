441b96b1a45217ac32daed56753a63384121a920
Poojan <poojansheth999@gmail.com>
2026-08-25 21:42:54 +0530

    add 3.7

--- lab3_07.cpp
+++ lab3_07.cpp
@@ -1,8 +1,25 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     
+    string s;
+    cin>> s;
+ 
+    int count = 0;
+    
+    for(int i = 0; i < s.length(); i++){
+        if (s[i] == '1'){ 
+            for (int j = i; j < s.length(); j++) {
+                if (s[j] == '1') {
+                    count++;
+                }
+            }
+        }
+    }
+        
+    cout<<count<<endl;
     
     return 0;
     
